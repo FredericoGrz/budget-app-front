eed31749574ff5f4c8745fd4f1b819064d5cdc89
Frederico Graziani <frederico.graz@gmail.com>
2024-07-03 13:38:39 -0300

    Criado o arquivo de conexão com a api

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const apiUrl = import.meta.env.BACKEND_BASE_URL;
+
+export const api = axios.create({
+  baseURL: apiUrl
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("@budgetapp:token");
+      localStorage.removeItem("@budgetapp:user");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
