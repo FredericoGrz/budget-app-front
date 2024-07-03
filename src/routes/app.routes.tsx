b23ad57438bf558fa56f2db464f3babd9c7a758e
Frederico Graziani <frederico.graz@gmail.com>
2024-07-03 13:40:13 -0300

    Criado os arquivos de rotas auth e app

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/settings" element={<Settings />} /> */}
+    </Routes>
+  );
+}
