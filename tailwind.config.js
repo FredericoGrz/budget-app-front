cb0c67a974f95d982ef7073488d12e8f0c904732
Frederico Graziani <frederico.graz@gmail.com>
2024-07-03 11:31:37 -0300

    Criado um breakpoint para xs a partir de 400px + sombra a direita e a esquerda

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "400px",
+      },
+      boxShadow: {
+        right: "5px 0px 10px rgba(0, 0, 0, 0.1)",
+        left: "-5px 0px 10px rgba(0, 0, 0, 0.1)",
+      },
+    },
   },
   plugins: [],
 };
