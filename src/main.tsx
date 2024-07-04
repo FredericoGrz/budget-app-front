7d98adaf6884710f09888dc6dd2de57b7ae99fc1
Frederico Graziani <frederico.graz@gmail.com>
2024-07-04 16:19:19 -0300

    Adicionado o toaster na aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from "./hooks/use-auth.tsx";
 import { Routes } from "./routes/index.tsx";
+import { Toaster } from "./components/ui/toaster.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <Routes />
+      <Toaster />
     </AuthProvider>
   </React.StrictMode>
 );
