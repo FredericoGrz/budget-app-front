35e78a986744847f596b2a5f48e9b03721065267
Frederico Graziani <frederico.graz@gmail.com>
2024-07-04 17:57:15 -0300

    Dashboard da aplicação inicializada

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+
+function Dashboard() {
+  return (
+    <div className="min-h-screen w-full bg-zinc-50 flex flex-col">
+      <Header />
+    </div>
+  );
+}
+
+export default Dashboard;
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
 
 export function AppRoutes() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/settings" element={<Settings />} /> */}
+      <Route path="/" element={<Dashboard />} />
     </Routes>
   );
 }
