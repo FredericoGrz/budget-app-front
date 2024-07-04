78f8e37125fc825e89e16b84a54b9e2f1a603c86
Frederico Graziani <frederico.graz@gmail.com>
2024-07-04 16:19:08 -0300

    Adicionado o método de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,8 +2,36 @@ import { Input } from "../components/Input";
 import { LuPiggyBank } from "react-icons/lu";
 import jacare from "../assets/jacare-regando-planta.png";
 import { Link } from "react-router-dom";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuth } from "../hooks/use-auth";
+
+interface IFormInputs {
+  email: string;
+  password: string;
+}
+
+const schema = yup.object().shape({
+  email: yup.string().email("Formato inválido").required("Email é obrigatório"),
+  password: yup.string().required("Senha é obrigatória"),
+});
 
 function Login() {
+  const { signIn } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (user: IFormInputs) => {
+    signIn(user);
+  };
+
   return (
     <div className="h-screen grid grid-cols-1 lg:grid-cols-12">
       <div className="bg-zinc-50 lg:col-span-4 lg:z-10 lg:shadow-right flex flex-col justify-evenly items-center p-4">
@@ -13,13 +41,28 @@ function Login() {
             Welcome to Budget Web App
           </h1>
         </div>
-        <form className="flex flex-col gap-8 w-full p-4">
-          <Input label="Email" type="email" required />
-          <Input label="Password" type="password" required />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-8 w-full p-4"
+        >
+          <Input
+            label="Email"
+            type="email"
+            required
+            {...register("email")}
+            error={errors.email ? errors.email.message : ""}
+          />
+          <Input
+            label="Password"
+            type="password"
+            required
+            {...register("password")}
+            error={errors.password ? errors.password.message : ""}
+          />
         </form>
         <div className="flex flex-col gap-5 px-4 w-full">
           <button
-            type="button"
+            onClick={handleSubmit(onSubmit)}
             className="text-zinc-50 bg-zinc-800 p-4 text-center rounded-3xl"
           >
             Login
