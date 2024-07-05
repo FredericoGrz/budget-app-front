79a517fbdfe8817770b1774323704f726f3ce9d0
Frederico Graziani <frederico.graz@gmail.com>
2024-07-05 15:22:35 -0300

    Adicionado o budget e os gastos no Header (versão desktop)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
 import { useAuth } from "../hooks/use-auth";
 import { Avatar } from "./Avatar";
+import { StatusBudgetHeader } from "./StatusBudgetHeader";
 
 export function Header() {
   const { user } = useAuth();
   return (
-    <div className="flex items-center justify-between p-4">
+    <div className="flex items-center justify-between xl:px-8 p-4">
       <div className="flex flex-col gap-4">
-        <p className="text-3xl">Budget App</p>
-        {user?.name && <p className="text-zinc-700">Welcome {user.name}!</p>}
+        <p className="text-3xl text-violet-700">Budget App</p>
+        {user?.name && <p className="text-violet-500">Welcome {user.name}!</p>}
       </div>
+      <StatusBudgetHeader className="hidden lg:flex items-center gap-10" />
       <Avatar />
     </div>
   );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,14 @@
+import { CardMoney } from "../components/CardMoney";
 import { Header } from "../components/Header";
 
 function Dashboard() {
   return (
     <div className="min-h-screen w-full bg-zinc-50 flex flex-col">
       <Header />
+      <div className="flex-1 grid grid-cols-2 gap-2 p-4 lg:hidden">
+        <CardMoney type="budget" className="col-span-1 h-32" />
+        <CardMoney type="spent" className="col-span-1 h-32" />
+      </div>
     </div>
   );
 }
