6b109433eb60b149400cabfd659e0eda7fae1e9c
Frederico Graziani <frederico.graz@gmail.com>
2024-07-11 12:59:54 -0300

    Adicionado headers na conexao com a api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 
 export const api = axios.create({
-  baseURL: apiUrl
+  baseURL: apiUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 api.interceptors.response.use(
